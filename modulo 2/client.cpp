dc03124b4ae458a9e706fbff8ef156692ec14de6
Marcus Huziwara <marcushuziwara@usp.br>
2022-07-18 17:35:44 -0300

    comandos quit e connect

--- modulo 2/client.cpp
+++ modulo 2/client.cpp
@@ -27,43 +27,57 @@ void send_message(int client_socket);
 void recv_message(int client_socket);
 
 int main()
-{
-	if((client_socket=socket(AF_INET,SOCK_STREAM,0))==-1)
-	{
-		perror("socket: ");
-		exit(-1);
+{	
+	string command;
+	while(1){
+		
+		cout << "Digite o comando:" << endl;
+		cin >> command;
+		cin.clear();
+
+
+		if(command == "/connect"){
+
+			if((client_socket=socket(AF_INET,SOCK_STREAM,0))==-1)
+			{
+				perror("socket: ");
+				exit(-1);
+			}
+
+			struct sockaddr_in client;
+			client.sin_family=AF_INET;
+			client.sin_port=htons(10000); // Port no. of server
+			client.sin_addr.s_addr=INADDR_ANY;
+			//client.sin_addr.s_addr=inet_addr("127.0.0.1"); // Provide IP address of server
+			bzero(&client.sin_zero,0);
+
+			if((connect(client_socket,(struct sockaddr *)&client,sizeof(struct sockaddr_in)))==-1)
+			{
+				perror("connect: ");
+				exit(-1);
+			}
+			signal(SIGINT, catch_ctrl_c);
+			char name[MAX_LEN];
+			cout<<"Enter your name : ";
+			cin >> name;
+			send(client_socket,name,sizeof(name),0);
+
+			cout<<colors[NUM_COLORS-1]<<"\n\t  ====== Welcome to the chat-room ======   "<<endl<<def_col;
+
+			thread t1(send_message, client_socket);
+			thread t2(recv_message, client_socket);
+
+			t_send=move(t1);
+			t_recv=move(t2);
+
+			if(t_send.joinable())
+				t_send.join();
+			if(t_recv.joinable())
+				t_recv.join();
+			
+		} 
+		
 	}
-
-	struct sockaddr_in client;
-	client.sin_family=AF_INET;
-	client.sin_port=htons(10000); // Port no. of server
-	client.sin_addr.s_addr=INADDR_ANY;
-	//client.sin_addr.s_addr=inet_addr("127.0.0.1"); // Provide IP address of server
-	bzero(&client.sin_zero,0);
-
-	if((connect(client_socket,(struct sockaddr *)&client,sizeof(struct sockaddr_in)))==-1)
-	{
-		perror("connect: ");
-		exit(-1);
-	}
-	signal(SIGINT, catch_ctrl_c);
-	char name[MAX_LEN];
-	cout<<"Enter your name : ";
-	cin.getline(name,MAX_LEN);
-	send(client_socket,name,sizeof(name),0);
-
-	cout<<colors[NUM_COLORS-1]<<"\n\t  ====== Welcome to the chat-room ======   "<<endl<<def_col;
-
-	thread t1(send_message, client_socket);
-	thread t2(recv_message, client_socket);
-
-	t_send=move(t1);
-	t_recv=move(t2);
-
-	if(t_send.joinable())
-		t_send.join();
-	if(t_recv.joinable())
-		t_recv.join();
 			
 	return 0;
 }
@@ -102,15 +116,16 @@ void send_message(int client_socket)
 	{
 		cout<<colors[1]<<"You : "<<def_col;
 		char str[MAX_LEN];
-		cin.getline(str,MAX_LEN);
+		cin >> str;
 		send(client_socket,str,sizeof(str),0);
-		if(strcmp(str,"#exit")==0)
+		if(strcmp(str,"/quit")==0)
 		{
-			exit_flag=true;
-			t_recv.detach();	
-			close(client_socket);
-			return;
+				exit_flag=true;
+				t_recv.detach();	
+				close(client_socket);
+				return;
 		}	
+		
 	}		
 }
 
