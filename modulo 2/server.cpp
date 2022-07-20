7ea121c5bbb9bf36f2706806592eae7b204166df
Thales Darini <thalesdarini@gmail.com>
2022-07-20 17:17:21 -0300

    server shutdown and warn clients + broadcast fix

--- modulo 2/server.cpp
+++ modulo 2/server.cpp
@@ -21,14 +21,14 @@ struct terminal
 	thread th;
 };
 
-//int server_socket;
+int server_socket;
 vector<terminal> clients;
 string def_col="\033[0m";
 string colors[]={"\033[31m", "\033[32m", "\033[33m", "\033[34m", "\033[35m","\033[36m"};
 int seed=0;
 mutex cout_mtx,clients_mtx;
 
-//void catch_ctrl_c(int signal);
+void catch_ctrl_c(int signal);
 string color(int code);
 void set_name(int id, char name[]);
 void shared_print(string str, bool endLine = true);
@@ -39,7 +39,6 @@ void handle_client(int client_socket, int id);
 
 int main()
 {
-	int server_socket;
 	if((server_socket=socket(AF_INET,SOCK_STREAM,0))==-1)
 	{
 		perror("socket: ");
@@ -68,15 +67,16 @@ int main()
 	int client_socket;
 	unsigned int len=sizeof(sockaddr_in);
 
-	//signal(SIGINT, catch_ctrl_c);
+	signal(SIGINT, catch_ctrl_c);
 	cout<<colors[NUM_COLORS-1]<<"\n\t  ====== Welcome to the chat-room ======   "<<endl<<def_col;
 
 	while(1)
 	{
 		if((client_socket=accept(server_socket,(struct sockaddr *)&client,&len))==-1)
 		{
-			perror("accept error: ");
-			exit(-1);
+			//perror("accept error: ");
+			cout << "Stopped accepting new connections" << endl;
+			break;
 		}
 		seed++;
 		thread t(handle_client,client_socket,seed);
@@ -92,28 +92,35 @@ int main()
 		);
 	}
 
-	/*
+	// Joins threads
+	lock_guard<mutex> guard(clients_mtx);
 	for(int i=0; i<clients.size(); i++)
 	{
-		clients[i].th.detach();
-		shutdown(clients[i].socket, SHUT_RDWR);
-		close(clients[i].socket);
+		if (clients[i].th.joinable())
+			clients[i].th.join();
 	}
-	*/
 
-	close(server_socket);
+	if (server_socket != -1) 
+		close(server_socket);
+
 	return 0;
 }
 
-/*
+
 // Handler for "Ctrl + C"
 void catch_ctrl_c(int signal) 
 {
-	cout << "Closing server and terminating program..." << endl;
-	close(server_socket);
+	cout << "\33[2K\rClosing server and terminating program..." << endl;
+	lock_guard<mutex> guard(clients_mtx);
+	for(int i=0; i<clients.size(); i++)
+	{
+		shutdown(clients[i].socket, SHUT_RDWR);
+		close(clients[i].socket);
+	}
 
-	//exit(signal);
-}*/
+	close(server_socket);
+	server_socket = -1;
+}
 
 string color(int code)
 {
@@ -146,13 +153,15 @@ void shared_print(string str, bool endLine)
 int broadcast_message(string message, int sender_id)
 {
 	lock_guard<mutex> guard(clients_mtx);
-	char temp[MAX_LEN];
-	strcpy(temp,message.c_str());
+	//cout << "message being sent: " << message << endl; 
+	//char temp[MAX_LEN];
+	//memset(temp, 0, MAX_LEN);
+	//strcpy(temp,message.c_str());
 	for(int i=0; i<clients.size(); i++)
 	{
 		if(clients[i].id!=sender_id)
 		{
-			send(clients[i].socket,temp,sizeof(temp),0);
+			send(clients[i].socket,message.c_str(),message.length() + 1,0);
 		}
 	}	
 
@@ -163,13 +172,13 @@ int broadcast_message(string message, int sender_id)
 int send_message(string message, int sender_id)
 {
 	lock_guard<mutex> guard(clients_mtx);
-	char temp[MAX_LEN];
-	strcpy(temp,message.c_str());
+	//char temp[MAX_LEN];
+	//strcpy(temp,message.c_str());
 	for(int i=0; i<clients.size(); i++)
 	{
 		if(clients[i].id==sender_id)
 		{
-			send(clients[i].socket,temp,sizeof(temp),0);
+			send(clients[i].socket,message.c_str(),message.length() + 1,0);
 		}
 	}	
 
@@ -215,7 +224,7 @@ void handle_client(int client_socket, int id)
 
 	// Display welcome message
 	string welcome_message=string(name)+string(" has joined com id ") + to_string(id);
-	broadcast_message("#NULL",id);	
+	broadcast_message(string("#NULL"),id);	
 	broadcast_message(id,id);								
 	broadcast_message(welcome_message,id);	
 	shared_print(color(id)+welcome_message+def_col);
@@ -225,14 +234,13 @@ void handle_client(int client_socket, int id)
 		int bytes_received=recv(client_socket,str,sizeof(str),0);
 		if(bytes_received<=0){
 			cout << "returning thread on client " << to_string(id) << endl;
-			
 			return;
 		}
 		if(strcmp(str,"/quit")==0)
 		{
 			// Display leaving message
 			string message=string(name)+string(" has left");		
-			broadcast_message("#NULL",id);			
+			broadcast_message(string("#NULL"),id);			
 			broadcast_message(id,id);						
 			broadcast_message(message,id);
 			shared_print(color(id)+message+def_col);
@@ -241,7 +249,7 @@ void handle_client(int client_socket, int id)
 		}
 		if (strcmp(str, "/ping") == 0)
 		{
-			send_message("#PONG", id);
+			send_message(string("#PONG"), id);
 		}
 		else {
 			broadcast_message(string(name),id);					
