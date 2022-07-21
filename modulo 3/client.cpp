75c01de21248c708ab3648dc33a31ea9edc5ee16
Marcus Huziwara <marcushuziwara@usp.br>
2022-07-20 23:07:47 -0300

    comandos do admin

--- modulo 3/client.cpp
+++ modulo 3/client.cpp
@@ -192,6 +192,10 @@ void recv_message(int client_socket)
 		if(strcmp(name,"#PONG")==0){
 			cout<<"pong!"<<endl;
 		}
+		else if(strcmp(name, "#IP")==0){
+			
+			cout<<"ip"<<endl;
+		}
 		else{
 			recv(client_socket,&color_code,sizeof(color_code),0);
 			recv(client_socket,str,sizeof(str),0);
